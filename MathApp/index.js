e70c0630ab6d20940cf4f4af20d246c729768361
Xevurah <ali_524-@hotmail.com>
2024-03-29 21:23:36 -0600

    MathApp created

---
+++ MathApp/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Sum of two numbers
+     * 
+     * @param {*} n1 First number of the sum
+     * @param {*} n2 Second number of the sum
+     * @returns 
+     */
+    sum: function (n1,n2){
+        return n1 + n2;
+    },
+    /**
+     * Substraction of two numbers
+     * 
+     * @param {*} n1 First number of the substraction
+     * @param {*} n2 Second number of the substraction
+     * @returns 
+     */
+    substraction: function (n1,n2){
+        return n1 - n2;
+    },
+    /**
+     * Multiplication of two numbers
+     * 
+     * @param {*} n1 First number to multiply
+     * @param {*} n2 Second number to multiply
+     * @returns 
+     */
+    multiplication: function (n1,n2){
+        return n1 * n2;
+    },
+    /**
+     * Division of two numbers
+     * 
+     * @param {*} n1 First number to divide
+     * @param {*} n2 Second number to divide
+     * @returns 
+     */
+    division: function (n1,n2){
+        return n1 / n2;
+    }
+}
